6c35c1d99b54c619809ef23527f79a234c1e3ea2
Federico Nicolas Zamora <federicozamora616@gmail.com>
2025-01-14 12:40:46 -0300

    cambio de logo

--- src/components/LofoEco.jsx
+++ src/components/LofoEco.jsx
@@ -1,9 +1,13 @@
 import { Leaf } from "lucide-react";
 export const LeafLogo = () => {
   return (
-    <div className="mb-8">
+    <div className="mb-8 flex justify-center items-center">
       <div className="inline-block p-4 bg-white rounded-full shadow-md animate-leaf-bounce">
-        <Leaf className="w-8 h-8 text-ecoblue" />
+        <img
+          src="https://as1.ftcdn.net/jpg/02/92/51/28/1000_F_292512881_vbEW6X4uoEnqhBJTHEBxbrtSmRcBWuNH.webp"
+          alt="ecologia"
+          className="w-10 h-10 text-ecoblue"
+        />
       </div>
     </div>
   );
